5f4064836a075713dd993df774011ec36ed610f8
TimeFlyingAway <11231706+timeflyingaway@user.noreply.gitee.com>
2024-07-16 22:52:49 +0800

    Bug fix and add Cache-Control header

--- xml-sitemap.php
+++ xml-sitemap.php
@@ -59,6 +59,26 @@ class Joost_XML_Sitemap_PHP {
 	private array $replacements;
 
 	/**
+	 * URLs we replace with latest modified time.
+	 */
+	private array $replacelatestmod;
+
+	/**
+	 * Last modified date of all files.
+	 */
+	private int $last_mod_date = 0;
+
+	/**
+	 * The Cache-Control Header.
+	 */
+	private string $cache_control;
+
+	/**
+	 * The expire time used in header.
+	 */
+	private int $expire_time;
+
+	/**
 	 * Generates our XML sitemap.
 	 *
 	 * @return void
@@ -92,7 +112,10 @@ class Joost_XML_Sitemap_PHP {
 		$this->priority     = (float) $config['priority'];
 		$this->recursive    = (bool) $config['recursive'];
 		$this->replacements = (array) $config['replacements'];
+		$this->replacelatestmod = (array) $config['replacelatestmod'];
 		$this->xsl          = (string) $config['xsl'];
+		$this->cache_control = (string) $config['cache_control'];
+		$this->expire_time  = (int) $config['expire_time'];
 	}
 
 	/**
@@ -116,32 +139,44 @@ class Joost_XML_Sitemap_PHP {
 		$handle = opendir( $dir );
 
 		while ( false !== ( $file = readdir( $handle ) ) ) {
+			$directory = $dir . $file;
+
 			// Check if this file needs to be ignored, if so, skip it.
-			if ( in_array( utf8_encode( $file ), $this->ignore ) ) {
+			if ( in_array( iconv('ISO-8859-1', 'UTF-8//IGNORE', $directory ), $this->ignore ) || in_array( iconv('ISO-8859-1', 'UTF-8//IGNORE', $file ), $this->ignore ) ) {
 				continue;
 			}
 
-			if ( $this->recursive && is_dir( $dir . $file ) ) {
-				$this->parse_dir( $dir . $file . '/', $url . $file . '/' );
+			if ( $this->recursive && is_dir( $directory ) ) {
+				$this->parse_dir( $directory . '/', $url . $file . '/' );
+			}
+
+			// Create a W3C valid date for use in the XML sitemap based on the file modification time.
+			$file_mod_time = filemtime( $directory );
+			if ( ! $file_mod_time ) {
+				$file_mod_time = filectime( $directory );
+			}
+
+			if ( $file_mod_time > $this->last_mod_date ) {
+				$this->last_mod_date = $file_mod_time;
 			}
 
 			// Check whether the file has on of the extensions allowed for this XML sitemap.
-			$extension = pathinfo( $dir . $file, PATHINFO_EXTENSION );
+			$extension = pathinfo( $directory, PATHINFO_EXTENSION );
 			if ( empty( $extension ) || ! in_array( $extension, $this->filetypes ) ) {
 				continue;
 			}
 
-			// Create a W3C valid date for use in the XML sitemap based on the file modification time.
-			$file_mod_time = filemtime( $dir . $file );
-			if ( ! $file_mod_time ) {
-				$file_mod_time = filectime( $dir . $file );
+			if ( in_array( iconv('ISO-8859-1', 'UTF-8//IGNORE', $directory ), $this->replacelatestmod ) || in_array( iconv('ISO-8859-1', 'UTF-8//IGNORE', $file ), $this->replacelatestmod ) ) {
+				$mod = '${last_mod_date}';
+			} else {
+				$mod = date( 'c', $file_mod_time );
 			}
-
-			$mod = date( 'c', $file_mod_time );
 
 			// Replace the file with its replacement from the settings, if needed.
 			if ( isset( $this->replacements[ $file ] ) ) {
 				$file = $this->replacements[ $file ];
+			} else if ( isset( $this->replacements[ $directory ] ) ) {
+				$file = $this->replacements[ $directory ];
 			}
 
 			// Start creating the output
@@ -151,13 +186,12 @@ class Joost_XML_Sitemap_PHP {
 			if ( ! empty( $this->changefreq ) ) {
 				$output .= "\t" . '<changefreq>' . $this->changefreq . '</changefreq>' . PHP_EOL;
 			}
-			if ( $this->priority !== 0 && $this->priority <= 1 ) {
+			if ( $this->priority != 0 && $this->priority <= 1 ) {
 				$output .= "\t" . '<priority>' . $this->priority . '</priority>' . PHP_EOL;
 			}
 			$output .= '</url>' . PHP_EOL;
 
 			$this->output .= $output;
-
 		}
 		closedir( $handle );
 	}
@@ -168,15 +202,24 @@ class Joost_XML_Sitemap_PHP {
 	 * @return void
 	 */
 	private function output(): void {
+		$last_mod = date( 'c', $this->last_mod_date );
+		$output = str_replace('${last_mod_date}', $last_mod, $this->output);
+
 		// Sent the correct header so browsers display properly, with or without XSL.
 		header( 'Content-Type: application/xml' );
+
+		$cache_control = str_replace(['$expire_time'], [$this->expire_time], $this->cache_control);
+		header('Cache-Control: ' . $cache_control);
+		if ( $this->expire_time > 0 ) {
+			header("Expires: " . gmdate("D, d M Y H:i:s", time() + $this->expire_time) . " GMT");
+		}
 
 		echo '<?xml version="1.0" encoding="utf-8"?>' . PHP_EOL;
 		if ( ! empty( $this->xsl ) ) {
 			echo '<?xml-stylesheet type="text/xsl" href="' . $this->url . $this->xsl . '"?>' . PHP_EOL;
 		}
 		echo '<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
-		echo $this->output;
+		echo $output;
 		echo '</urlset>' . PHP_EOL;
 	}
 }
